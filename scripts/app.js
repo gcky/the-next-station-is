1f56de12c10ff73648d6bb2932751b85df952743
gcky <gcky.cheng@gmail.com>
2018-05-27 12:12:22 +0100

    Implement infinite scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('theNextStation', ['ngAnimate']);
+var app = angular.module('theNextStation', ['ngAnimate', 'infinite-scroll']);
--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -1,5 +1,8 @@
 app.controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.stations = stations;
+    $scope.stations = stations.slice(0, 20);
+    $scope.loadMore = function() {
+        $scope.stations = stations.slice(0, $scope.stations.length + 5);
+    };
     angular.element(document).ready(function () {
         angular.element(document.querySelector('.loading-prompt')).addClass('loaded');
         angular.element(document.querySelector('.heavy')).addClass('loaded');
