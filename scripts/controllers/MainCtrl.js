ab7999d0e3181acb89b195709b28ceeb05d8d176
gcky <gcky.cheng@gmail.com>
2018-05-28 21:36:04 +0100

    Modify on grid load event

--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -3,9 +3,9 @@ app.controller('MainCtrl', ['$scope', function ($scope) {
     $scope.loadMore = function() {
         $scope.stations = stations.slice(0, $scope.stations.length + 5);
     };
-    angular.element(document).ready(function () {
+    $scope.onGridLoad = function () {
         angular.element(document.querySelector('.loading-prompt')).addClass('loaded');
         angular.element(document.querySelector('.heavy')).addClass('loaded');
-    });
+    }
     $scope.randomStation = stations[Math.floor(Math.random()*stations.length)];
 }]);
